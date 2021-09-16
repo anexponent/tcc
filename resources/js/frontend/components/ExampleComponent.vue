cde9fd7f61d50e488d17d16b0529cf4132358403
Azeez Adesina <azeezadesina@yahoo.com>
2021-09-16 14:44:00 +0100

    name changed

--- resources/js/frontend/components/ExampleComponent.vue
+++ resources/js/frontend/components/ExampleComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <div>
             <img :src="'./img/brand/citizen.png'" height="450" width="600">
-            <h3 style="margin-top: 0 !important;"><small>TCC EAGLES EMPOWERMENT PROGRAM</small></h3>
+            <h3 style="margin-top: 0 !important;"><small>TCC YOUTH CENTRE</small></h3>
         </div>
     </div>
 </template>
